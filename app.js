6ec807192927acb3bd1936f47fc06b94f8191092
andybardagi <andresbardagi2002@gmail.com>
2021-03-21 19:09:14 -0300

    added MySQL to NPM, config app.js and depositos-frontend. TODO:DToDTo,

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const _ =  require('lodash')
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 
+const _ =  require('lodash');
+
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+
+//middlewares: instrucciones que deben ejecutarse antes de las peticiones de los usuarios
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(morgan('dev')); //con el parámetro dev nos muestra por consola las solicitudes que hacemos al servidor
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user:'',
+    password:'',
+    port: 3306,
+    database: 'stockappdatabase' 
+}, 'single'));
 
+
+//routes
 app.listen(3000, function(){
     console.log("server ON");
-})
+});
 
 app.get('/', function(req, res){
-    res.render("home");
+    res.render("home",
+    {title: 'Home'});
 });
 
 app.get('/depositos', function(req, res){
-    res.render("depositos");
+    res.render("depositos", {
+        title: "Depósitos"
+    });
 });
 
 app.get('/ventas', function(req, res){

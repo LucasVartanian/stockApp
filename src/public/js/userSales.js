920833293df6e2435458959d4f423c57367fa2bf
andybardagi <andresbardagi2002@gmail.com>
2021-04-02 18:02:22 -0300

    ventas acumuladas

--- src/public/js/userSales.js
+++ src/public/js/userSales.js
@@ -1,1 +1,35 @@
-date = document.querySelector()
+const date = document.querySelector("input[name='date']")
+const prodCode = document.querySelector("input[name='prodCode']")
+const prodPrice = document.querySelector("input[name='prodPrice']")
+const quant = document.querySelector("input[name='quant']")
+var sales = []
+
+function validAll(){
+    if (prodCode.value == ""){
+        prodCode.setCustomValidity('El producto no puede no existir')
+        return false
+    } else{
+        prodCode.setCustomValidity('')
+    }
+
+    if (prodPrice.value=='' || prodPrice.value<=0){
+        prodPrice.setCustomValidity('El precio debe ser mayor a cero')
+        return false
+    } else{
+        prodPrice.setCustomValidity('')
+    }
+
+    return true
+}
+
+function addSale() {
+    if (validAll()){
+        sales.push({
+            date: date.value,
+            prodCode: prodCode.value,
+            prodPrice: prodPrice.value,
+            quant: quant.value
+        }
+        )
+    }
+}

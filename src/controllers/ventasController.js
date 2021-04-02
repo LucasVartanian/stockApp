3529c942512a8e3eb1633e438063f18aea01d8bc
LucasVartanian <lucasvarta@gmail.com>
2021-04-02 16:44:41 -0300

    ventasController

--- src/controllers/ventasController.js
+++ src/controllers/ventasController.js
@@ -1,20 +1,32 @@
 const ventasController = {};
 
 ventasController.save = (req, res) => {
-    req.getConnection((err, conn) =>{
-        const fecha = parseInt(req.body.fecha);
-        const prod = req.body.prodVenta;
-        const cantidad = parseInt(req.body.quantVenta);
-        const monto = parseInt(req.body.montoProd);
-        const dataVentas = [fecha, prod, cantidad, monto];
+    req.getConnection((err, conn) => {
+            const fecha = (req.body.fecha);
+            const prod = req.body.prodVenta;
+            const cantidad = parseInt(req.body.quantVenta);
+            const monto = parseInt(req.body.montoProd);
 
-        
-        if(!err){
-            console.log(dataVentas);
-            res.redirect('/ventas');
+            const dataVentas = [{
+                fecha,
+                prod,
+                cantidad,
+                monto
+            }];
+
+            dataVentas.map((prod) => {
+                conn.query("INSERT INTO 'sales' (?) VALUES (?); ", (err, rows) => {
+                    if (!err) {
+                        console.log(dataVentas);
+                        res.redirect('/ventas');
+                    }
+                });
+            })
         }
-    })   
+
+    )
 }
+
 
 
 module.exports = ventasController;

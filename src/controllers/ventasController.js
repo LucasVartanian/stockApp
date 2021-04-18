8199bec045d62dd39f654b456025744efe3146e9
andybardagi <andresbardagi2002@gmail.com>
2021-04-18 17:29:17 -0300

    Funciona ADD Ventas

--- src/controllers/ventasController.js
+++ src/controllers/ventasController.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const ventasController = {};
 
 ventasController.main = (req, res) => {
@@ -22,20 +24,23 @@ ventasController.main = (req, res) => {
 ventasController.save = (req, res) => {
     req.getConnection((err, conn) => {
 
-        const nuevaVenta = req.body;
-        const dataVentas = [nuevaVenta];
+        const dataVentas = JSON.parse(req.params.sales);
 
         dataVentas.map((prod) => {
             console.log(prod);
             const valores = Object.values(prod);
-            conn.query("INSERT INTO sales (date, prodCode, quant, prodPrice) VALUES (?); ", [valores], (err, rows) => {
+            conn.query('SELECT * FROM stock where descr = ?;', valores[1], (err, producto) => {
                 if (err) {
                     res.json(err)
-                }else{
-                    res.redirect('/ventas');
+                } else {
+                    valores.push(producto[0].code)
+                    conn.query("INSERT INTO sales (date, prodDescr, quant, prodPrice, prodCode) VALUES (?); ", [valores]);
                 }
-            });
+            })
+            
         });
+
+        res.redirect('/ventas')
     });
 }
 
